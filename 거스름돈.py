cdf2ea852173fc26544336672e856c681cc2944b
kor-tk <tykim456@naver.com>
2023-11-24 04:44:56 +0900

    Update 거스름돈.py

--- 거스름돈.py
+++ 거스름돈.py
@@ -6,4 +6,9 @@ for i in range(len(coin)):
     count = count + n // coin[i]   # count += n // coin[i]
     n = n % coin[i]   # n %= coin[i]
 
+'''  # 이 방법으로도 된다
+for i in coin:
+    count += n // coin
+    n %= coin
+'''
 print("최소 거스름돈 개수 : {0}".format(count))
